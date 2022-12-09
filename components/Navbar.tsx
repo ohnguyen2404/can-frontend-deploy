9a22eec19b753936d111c9882a199caad3355ec7
On Hao Nguyen <ohn24042000@gmail.com>
2022-12-09 10:50:01 +0700

    CAN-4 Initial logo images, Initial components folder, Initial styles.ts common file, Initial constants folder, Apply common css configuration for the app, implement Navbar component (without animation)

---
+++ components/Navbar.tsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react"
+import Image from "next/image"
+import styles from "../styles"
+import { navLinks, NAVBAR_HEIGHT } from "../constants"
+import { useRouter } from "next/router"
+
+const Navbar = () => {
+  const router = useRouter()
+  const currentPath = router.asPath
+
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [isScrollUp, setIsScrollUp] = useState(true)
+
+  const handleScroll = () => {
+    const currentScrollPos = window.scrollY
+
+    if (currentScrollPos > prevScrollPos) {
+      setIsScrollUp(false)
+    } else {
+      setIsScrollUp(true)
+    }
+
+    setPrevScrollPos(currentScrollPos)
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return () => window.removeEventListener("scroll", handleScroll)
+  })
+
+  return prevScrollPos < NAVBAR_HEIGHT ? (
+    // The initial navbar
+    <div className={`bg-transparent px-8 py-2 ${styles.flexCenter} border-b-2 border-white`}>
+      <nav className={`w-full flex justify-between items-center`}>
+        <Image src={"/logo_white.png"} alt="logo" width={75} height={59} />
+        <ul className="flex flex-row flex-1 items-center justify-end">
+          {navLinks.map((link, index) => (
+            <li
+              className={`uppercase cursor-pointer text-xl text-white font-normal ${
+                index === navLinks.length - 1 ? "mr-0" : "mr-10"
+              } ${currentPath.includes(link.id) && "underline underline-offset-8"}`}
+              key={link.id}
+            >
+              <a
+                onClick={() => {
+                  router.push(`#${link.id}`)
+                }}
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  ) : (
+    <div
+      className={`w-full flex flex-col ${styles.flexCenter} ${isScrollUp ? "sticky top-0 " : ""}`}
+    >
+      {/*The mutated navbar when page is scroll down*/}
+      <div className={`bg-white px-8 py-2 ${styles.flexCenter} nav-bar w-full`}>
+        <nav className={`w-full flex justify-between items-center`}>
+          <Image src={"/logo_red.png"} alt="logo" width={75} height={59} />
+          <ul className="flex flex-row flex-1 items-center justify-end">
+            {navLinks.map((link, index) => (
+              <li
+                className={`uppercase cursor-pointer text-xl text-black font-semibold ${
+                  index === navLinks.length - 1 ? "mr-0" : "mr-10"
+                } ${currentPath.includes(link.id) && "underline underline-offset-8"}`}
+                key={link.id}
+              >
+                <a
+                  onClick={() => {
+                    router.push(`#${link.id}`)
+                  }}
+                >
+                  {link.title}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+      <div className={`bg-red-600 w-[94%] h-[6px] ${styles.flexCenter}`}></div>
+    </div>
+  )
+}
+
+export default Navbar
