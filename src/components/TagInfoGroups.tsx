7eaf41541b7462570ba6915691bc461713011049
On Hao Nguyen <ohn24042000@gmail.com>
2022-12-12 14:54:56 +0700

    CAN-6 Finish implement animation when hover on tag info group

--- src/components/TagInfoGroups.tsx
+++ src/components/TagInfoGroups.tsx
@@ -4,38 +4,38 @@ import styles from "../styles"
 const TagInfoGroups = () => {
   const containerStyle = `flex flex-col flex-1 justify-between ${styles.flexCenter} rounded-lg tag-info-group shadow-lg`
   const paragraphStyle =
-    "text-center border-[1px] border-t-gray-400 border-transparent my-8 pt-2 w-[90%] mx-6 text-xl"
+    "text-center border-[1px] border-t-gray-400 border-transparent my-8 pt-2 w-[90%] text-xl truncate"
 
   const H2Style = "my-8 text-3xl font-semibold"
   return (
     <section id="tag-info-groups" className={`w-full ${styles.flexCenter} px-6 py-10 bg-white`}>
-      <div className={`${containerStyle} bg-lightBlue mr-6 rounded-lg`}>
+      <div className={`${containerStyle} bg-lightBlue blue-container mr-6 rounded-lg min-h-[260px]`}>
         <h2 className={H2Style}>Thị trường</h2>
-        <img src="/market.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/market.png" className={`object-contain max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
-          <p>Việt Nam, Phillipine, Ấn Độ,</p>
-          <p>Trung Quốc, Hong Kong</p>
+          <div>Việt Nam, Phillipine, Ấn Độ,</div>
+          <div>Trung Quốc, Hong Kong</div>
         </div>
       </div>
-      <div className={`${containerStyle} bg-lightPink mr-6 rounded-lg`}>
+      <div className={`${containerStyle} bg-lightPink pink-container mr-6 rounded-lg`}>
         <h2 className={H2Style}>Khách hàng</h2>
-        <img src="/customer.png" className={`object-fit max-w-[240px] max-h-[260px]`} />
+        <img src="/customer.png" className={`object-contain max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>Hỗ trợ hơn 10 000</p>
           <p>khách hàng</p>
         </div>
       </div>
-      <div className={`${containerStyle} bg-lightBlue mr-6 rounded-lg`}>
+      <div className={`${containerStyle} bg-lightBlue blue-container mr-6 rounded-lg`}>
         <h2 className={H2Style}>Hồ sơ</h2>
-        <img src="/profile.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/profile.png" className={`object-contain max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>Hơn 3000 hồ sơ</p>
           <p>nhận được PR</p>
         </div>
       </div>
-      <div className={`${containerStyle} bg-lightPink mr-0 rounded-lg`}>
+      <div className={`${containerStyle} bg-lightPink pink-container mr-0 rounded-lg`}>
         <h2 className={H2Style}>Tỷ lệ</h2>
-        <img src="/percent.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/percent.png" className={`object-contain max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>97% tỷ lệ đậu hồ sơ PR</p>
           <p>và Visa du học</p>
