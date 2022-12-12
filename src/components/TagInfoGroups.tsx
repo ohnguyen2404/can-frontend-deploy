80139f7afba2aa083df88db4968dd1a0f008a804
On Hao Nguyen <ohn24042000@gmail.com>
2022-12-12 10:19:14 +0700

    CAN-6 Import images for tag info groups and implement hover animation

--- src/components/TagInfoGroups.tsx
+++ src/components/TagInfoGroups.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import styles from "../styles"
 
 const TagInfoGroups = () => {
-  const containerStyle = `flex flex-col flex-1 justify-between ${styles.flexCenter} rounded-lg`
+  const containerStyle = `flex flex-col flex-1 justify-between ${styles.flexCenter} rounded-lg tag-info-group shadow-lg`
   const paragraphStyle =
     "text-center border-[1px] border-t-gray-400 border-transparent my-8 pt-2 w-[90%] mx-6 text-xl"
 
@@ -11,7 +11,7 @@ const TagInfoGroups = () => {
     <section id="tag-info-groups" className={`w-full ${styles.flexCenter} px-6 py-10 bg-white`}>
       <div className={`${containerStyle} bg-lightBlue mr-6 rounded-lg`}>
         <h2 className={H2Style}>Thị trường</h2>
-        <img src="/test_info.svg" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/market.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>Việt Nam, Phillipine, Ấn Độ,</p>
           <p>Trung Quốc, Hong Kong</p>
@@ -19,7 +19,7 @@ const TagInfoGroups = () => {
       </div>
       <div className={`${containerStyle} bg-lightPink mr-6 rounded-lg`}>
         <h2 className={H2Style}>Khách hàng</h2>
-        <img src="/test_info.svg" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/customer.png" className={`object-fit max-w-[240px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>Hỗ trợ hơn 10 000</p>
           <p>khách hàng</p>
@@ -27,7 +27,7 @@ const TagInfoGroups = () => {
       </div>
       <div className={`${containerStyle} bg-lightBlue mr-6 rounded-lg`}>
         <h2 className={H2Style}>Hồ sơ</h2>
-        <img src="/test_info.svg" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/profile.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>Hơn 3000 hồ sơ</p>
           <p>nhận được PR</p>
@@ -35,7 +35,7 @@ const TagInfoGroups = () => {
       </div>
       <div className={`${containerStyle} bg-lightPink mr-0 rounded-lg`}>
         <h2 className={H2Style}>Tỷ lệ</h2>
-        <img src="/test_info.svg" className={`object-fit max-w-[300px] max-h-[260px]`} />
+        <img src="/percent.png" className={`object-fit max-w-[300px] max-h-[260px]`} />
         <div className={paragraphStyle}>
           <p>97% tỷ lệ đậu hồ sơ PR</p>
           <p>và Visa du học</p>
