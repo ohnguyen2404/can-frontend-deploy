840be764e0a866fcaa023f7249c9dd51af887055
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2022-12-30 17:01:40 +0700

    CAN-5 Add title banner and styles.

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -16,9 +16,20 @@ const Banner: TComponent & TSubComponent = () => {
 				muted
 				loop
 				playsInline></video>
-			<div className="sash-ribbon-wrap w-full h-[7vw] bg-white absolute bottom-[10%] flex flex-row overflow-hidden">
+			<div className="sash-ribbon-wrap w-full h-[10%] bg-white absolute bottom-[10%] flex flex-row overflow-hidden">
 				<Banner.SashRibbon />
 				<Banner.SashRibbon />
+			</div>
+			<div className="title-container flex flex-col text-white absolute top-[20%] right-[10%] text-4xl font-medium">
+				<span className="title">
+					Start your new journey
+				</span>
+				<span className="title">
+					in Canada with
+				</span>
+				<span className="title">
+					Can Immigration Vietnam
+				</span>
 			</div>
 		</div>
 	);
