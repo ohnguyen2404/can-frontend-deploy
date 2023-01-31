65b698233d93749e0cde727ce6a8c5e57794e44a
On Hao Nguyen <ohn24042000@gmail.com>
2023-01-31 21:17:42 +0700

    CAN-16 Finish implementing NewsGroup

--- src/components/NewsGroup/NewsCard.tsx
+++ src/components/NewsGroup/NewsCard.tsx
@@ -2,12 +2,17 @@ import React from "react";
 
 export type TNewsCard = {
 	imgUrl: string;
+	isSelected: boolean;
+	handleOnClick: () => void;
+	newsLength: number;
+	idx: number;
 };
 
-const NewsCard = ({ imgUrl }: TNewsCard) => {
+const NewsCard = ({ imgUrl, isSelected, handleOnClick, newsLength, idx }: TNewsCard) => {
 	return (
 		<img
-			className="w-[332px] h-[380px] object-fit mr-2"
+			onClick={handleOnClick}
+			className={`w-[332px] h-[380px] object-fit ${idx !== newsLength - 1 && "mr-2"} ${isSelected && "selected-news-card"} cursor-pointer opacity-60`}
 			src={imgUrl}
 		/>
 	);
--- src/components/NewsGroup/NewsGroup.tsx
+++ src/components/NewsGroup/NewsGroup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
 import { TComponent } from "../../utils/types";
 import CircleButton from "../Buttons/CircleButton";
 import LongButton from "../Buttons/LongButton";
@@ -7,54 +7,117 @@ import NewsCard from "./NewsCard";
 
 const MOCK_NEWS = [
 	{
+		title: "SUMMER-CAMP-2023-01-1",
 		imgUrl: "/SUMMER-CAMP-2023-01.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-02-1",
 		imgUrl: "/SUMMER-CAMP-2023-02.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-03-1",
 		imgUrl: "/SUMMER-CAMP-2023-03.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-04-1",
 		imgUrl: "/SUMMER-CAMP-2023-04.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-01-2",
 		imgUrl: "/SUMMER-CAMP-2023-01.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-02-2",
 		imgUrl: "/SUMMER-CAMP-2023-02.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-03-2",
 		imgUrl: "/SUMMER-CAMP-2023-03.png",
 	},
 	{
+		title: "SUMMER-CAMP-2023-04-2",
+		imgUrl: "/SUMMER-CAMP-2023-04.png",
+	},
+	{
+		title: "SUMMER-CAMP-2023-01-3",
+		imgUrl: "/SUMMER-CAMP-2023-01.png",
+	},
+	{
+		title: "SUMMER-CAMP-2023-02-3",
+		imgUrl: "/SUMMER-CAMP-2023-02.png",
+	},
+	{
+		title: "SUMMER-CAMP-2023-03-3",
+		imgUrl: "/SUMMER-CAMP-2023-03.png",
+	},
+	{
+		title: "SUMMER-CAMP-2023-04-3",
 		imgUrl: "/SUMMER-CAMP-2023-04.png",
 	},
 ];
 
 const NewsGroup: TComponent = () => {
+	const [curIdx, setCurIdx] = useState(2);
+	const onClickPrev = () => {
+		if (curIdx > 0) {
+			setCurIdx((curIdx) => curIdx - 1);
+			if (curIdx < MOCK_NEWS.length - 2) {
+				document.getElementById("news-slider")!.scrollLeft -= 340;
+			}
+		}
+	};
+
+	const onClickNext = () => {
+		if (curIdx < MOCK_NEWS.length - 1) {
+			setCurIdx((curIdx) => curIdx + 1);
+			if (curIdx >= 2) {
+				document.getElementById("news-slider")!.scrollLeft += 340;
+			}
+		}
+	};
+
+	const onClickCard = (idx: number) => {
+		setCurIdx(idx);
+	};
+
+	console.log(curIdx);
+
 	return (
 		<section
 			id="news"
 			className="w-full mt-10">
 			<div className="flex flex-row justify-between">
 				<div className="ml-6 font-bold text-4xl uppercase">
-					<span>Top những lý do vì sao nên cho bé tham gia</span>
-					<div className="flex flex-row items-center">
-						<span className="pr-3">chương trình trại hè Canada 2023</span>
+					<span className="float-left">{MOCK_NEWS[curIdx].title.substring(0, MOCK_NEWS[curIdx].title.length - 1)}</span>
+					<span className="flex flex-row items-center float-right">
+						<span className="pr-3">{MOCK_NEWS[curIdx].title.substring(MOCK_NEWS[curIdx].title.length - 1)}</span>
 						<CircleButton />
-					</div>
+					</span>
 				</div>
 				<div className="flex flex-row mr-6 justify-between">
-					<LongButton direction={Direction.Left} />
-          <div className="w-4"></div>
-					<LongButton direction={Direction.Right} />
+					<LongButton
+						direction={Direction.Left}
+						handleOnClick={onClickPrev}
+					/>
+					<div className="w-4"></div>
+					<LongButton
+						direction={Direction.Right}
+						handleOnClick={onClickNext}
+					/>
 				</div>
 			</div>
 
-			<div className="w-full mt-48 relative h-[500px] flex flex-row">
-				{MOCK_NEWS.map(({ imgUrl }) => (
-					<NewsCard imgUrl={imgUrl} />
+			<div
+				id="news-slider"
+				className="w-full mt-16 min-h-[600px] items-center flex flex-row overflow-x-scroll scrollbar-hide">
+				{MOCK_NEWS.map(({ imgUrl }, idx) => (
+					<NewsCard
+						isSelected={idx === curIdx}
+						handleOnClick={() => onClickCard(idx)}
+						imgUrl={imgUrl}
+						idx={idx}
+						newsLength={MOCK_NEWS.length}
+					/>
 				))}
 			</div>
 		</section>
