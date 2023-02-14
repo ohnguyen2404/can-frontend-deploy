9e038d0db5e24702b3f5d96c96093099c85b4e70
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2023-02-14 14:30:16 +0700

    CAN-19 Update Image route and .gitignore.

--- src/pages/api/image.ts
+++ src/pages/api/image.ts
@@ -1,7 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import nextConnect from "next-connect";
 import multer from "multer";
-import { ERROR_CONTENT_LENGTH, ERROR_UNKNOWN, MAX_LISTFILE_SIZE, MAX_NUMBER_FILE } from "../../utils/settings";
+import { ERROR_CONTENT_LENGTH, ERROR_UNKNOWN, MAX_LISTFILE_SIZE, MAX_NUMBER_FILE, FILE_FORM_UPLOAD_NAME } from "../../utils/settings";
 
 export const config = {
 	api: {
@@ -9,9 +9,9 @@ export const config = {
 	},
 };
 
-interface NextConnectApiRequest extends NextApiRequest {
+type NextConnectApiRequest = NextApiRequest & {
 	images: Express.Multer.File[];
-}
+};
 
 const upload = multer({
 	limits: {
@@ -19,14 +19,16 @@ const upload = multer({
 		files: MAX_NUMBER_FILE,
 	},
 	storage: multer.diskStorage({
-		destination: "./public/uploads",
+		destination: (req, file, cb) => {
+			cb(null, "./public/uploads");
+		},
 		filename: (req, file, cb) => {
 			cb(null, file.originalname);
 		},
 	}),
 });
 
-const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
+export default nextConnect<NextApiRequest, NextApiResponse>({
 	onError(error, req, res) {
 		console.log("onError");
 		console.log(error);
@@ -36,25 +38,21 @@ const apiRoute = nextConnect<NextApiRequest, NextApiResponse>({
 		console.log("onNoMatch");
 		res.writeHead(405);
 	},
-});
-
-apiRoute.use((req, res, next) => {
-	const contentLength = req.headers["content-length"];
-	if (!contentLength || typeof Number(contentLength) !== "number") {
-		throw new Error(ERROR_UNKNOWN);
-	}
-	if (parseInt(contentLength) > MAX_LISTFILE_SIZE) {
-		throw new Error(ERROR_CONTENT_LENGTH);
-	}
-	next();
-});
-
-apiRoute.use(upload.array("image", MAX_NUMBER_FILE));
-
-apiRoute.post(async (request: NextConnectApiRequest, response) => {
-	response.status(200).json({
-		status: "oke",
+})
+	.use((req, res, next) => {
+		const contentLength = req.headers["content-length"];
+		if (!contentLength || typeof Number(contentLength) !== "number") {
+			throw new Error(ERROR_UNKNOWN);
+		}
+		if (parseInt(contentLength) > MAX_LISTFILE_SIZE) {
+			throw new Error(ERROR_CONTENT_LENGTH);
+		}
+		next();
+	})
+	.use(upload.array(FILE_FORM_UPLOAD_NAME))
+	.post(async (request: NextConnectApiRequest, response) => {
+		console.log(request);
+		response.status(200).json({
+			status: "oke",
+		});
 	});
-});
-
-export default apiRoute;
