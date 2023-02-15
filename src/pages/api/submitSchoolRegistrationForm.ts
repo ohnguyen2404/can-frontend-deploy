9921b412bce5e3861883e1d93d6082112bc6908b
phamgiahuy0501 <pham.giahuy.9041@gmail.com>
2023-02-16 03:42:46 +0700

    CAN-3 Update beautify submit School Registration Form route.

--- src/pages/api/submitSchoolRegistrationForm.ts
+++ src/pages/api/submitSchoolRegistrationForm.ts
@@ -18,46 +18,49 @@ type NextConnectApiRequest = NextApiRequest & {
 	files: Express.Multer.File[];
 };
 
-export default _nextConnect.use(guardContentLength, handleFiles(FILE_FORM_UPLOAD_NAME)).post(async (request: NextConnectApiRequest, response) => {
-	// request.emit("close");
-	const files = request.files;
-	const data: TSchoolRegistrationForm = request.body;
-	data.semester = JSON.parse((<unknown>data.semester) as string);
+export default _nextConnect
+	.use(guardContentLength)
+	.use(handleFiles(FILE_FORM_UPLOAD_NAME))
+	.post(async (request: NextConnectApiRequest, response) => {
+		// request.emit("close");
+		const files = request.files;
+		const data: TSchoolRegistrationForm = request.body;
+		data.semester = JSON.parse((<unknown>data.semester) as string);
 
-	if (!data.id) {
-		return response.status(400).json({ message: "Bad request" });
-	}
-
-	const attachments = files.map((file) => {
-		return {
-			filename: file.filename.replace(`${data.id}_`, ""),
-			path: file.path,
-		};
-	});
-
-	nodemailerTransporter
-		.sendMail({
-			from: process.env["MAIL_USER"],
-			to: process.env["MAIL_USER"],
-			html: html(SchoolRegistrationFormBody(data)),
-			subject: `Đăng ký trường học của "${data.name}"`,
-			attachments: attachments,
-		})
-		.then((res) => {
-			files.forEach((file) => {
-				unlink(file.path, (error) => {
-					if (error) {
-						console.log(error);
-						throw error;
-					}
-					console.log(`${file.path} was deleted`);
-				});
-			});
-			console.log(res);
-			return response.status(200).json({ success: true });
-		})
-		.catch((error) => {
-			console.log(error);
+		if (!data.id) {
 			return response.status(400).json({ message: "Bad request" });
+		}
+
+		const attachments = files.map((file) => {
+			return {
+				filename: file.filename.replace(`${data.id}_`, ""),
+				path: file.path,
+			};
 		});
-});
+
+		nodemailerTransporter
+			.sendMail({
+				from: process.env["MAIL_USER"],
+				to: process.env["MAIL_USER"],
+				html: html(SchoolRegistrationFormBody(data)),
+				subject: `Đăng ký trường học của "${data.name}"`,
+				attachments: attachments,
+			})
+			.then((res) => {
+				files.forEach((file) => {
+					unlink(file.path, (error) => {
+						if (error) {
+							console.log(error);
+							throw error;
+						}
+						console.log(`${file.path} was deleted`);
+					});
+				});
+				console.log(res);
+				return response.status(200).json({ success: true });
+			})
+			.catch((error) => {
+				console.log(error);
+				return response.status(400).json({ message: "Bad request" });
+			});
+	});
