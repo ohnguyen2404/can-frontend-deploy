fa70fd83b3736a22b43322ebba2a585733084a1c
Huy Pham <pham.giahuy.9041@gmail.com>
2023-02-22 02:08:48 +0700

    CAN-30 Update Banner component.

--- src/components/Banner/SashRibbon.tsx
+++ src/components/Banner/SashRibbon.tsx
@@ -1,111 +1,17 @@
+import { LIST_SASH_RIBBON_INFO } from "./setting";
+
 const SashRibbon = () => {
 	return (
 		<div className="sash-ribbon h-full flex shrink-0 relative">
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/CambrianCollege.jpeg"
-					alt="CambrianCollege.jpeg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/CapeBretonUniversity.jpg"
-					alt="CapeBretonUniversity.jpg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/CentennialCollege.png"
-					alt="CentennialCollege.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/DouglasCollege.jpg"
-					alt="DouglasCollege.jpg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/LangaraCollege.jpg"
-					alt="LangaraCollege.jpg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/BowValleyCollenge.png"
-					alt="BowValleyCollenge.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/ColumbiaCollege.png"
-					alt="ColumbiaCollege.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/UniversityofSaskatchewan.png"
-					alt="UniversityofSaskatchewan.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/GeorgeBrownCollege.png"
-					alt="GeorgeBrownCollege.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/UniversityofRegina.jpg"
-					alt="UniversityofRegina.jpg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/ThompsonRiversUniversity.png"
-					alt="ThompsonRiversUniversity.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/SaskatchewanPolytechnic.png"
-					alt="SaskatchewanPolytechnic.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/SAIT.jpg"
-					alt="SAIT.jpg"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/GeorgianCollege.png"
-					alt="GeorgianCollege.png"
-					className="h-1/2"
-				/>
-			</span>
-			<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
-				<img
-					src="assets/ribbon/CollegeofNewCaledonia.png"
-					alt="CollegeofNewCaledonia.png"
-					className="h-1/2"
-				/>
-			</span>
+			{LIST_SASH_RIBBON_INFO.map((item) => (
+				<span className="h-full w-fit flex shrink-0 items-center mx-[2vw]">
+					<img
+						src={item.imgSrc}
+						alt={item.imgAlt}
+						className="h-1/2"
+					/>
+				</span>
+			))}
 		</div>
 	);
 };
