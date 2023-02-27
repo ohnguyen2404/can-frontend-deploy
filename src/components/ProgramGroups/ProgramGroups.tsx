7e70bddc94a4fd0d088828e9a5bed9869dc6bfeb
Huy Pham <pham.giahuy.9041@gmail.com>
2023-02-28 02:09:14 +0700

    CAN-34 Update animation and layout related.

--- src/components/ProgramGroups/ProgramGroups.tsx
+++ src/components/ProgramGroups/ProgramGroups.tsx
@@ -1,10 +1,11 @@
 import { useRef } from "react";
-import { TComponent } from "../../utils/types";
+import { Direction, TComponent } from "../../utils/types";
 import ProgramCard, { TProgramCard } from "./ProgramCard";
 import gsap from "gsap/dist/gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { ITEM_IN_ROW, LIST_PROGRAM_INFO, WIDTH_ITEM } from "./setting";
 import { useIsomorphicLayoutEffect } from "../../utils/helper";
+import ProgramSubGroup from "./ProgramSubGroup";
 
 type TSubComponent = {
 	ProgramCard: typeof ProgramCard;
@@ -13,83 +14,61 @@ type TSubComponent = {
 const ProgramGroups: TComponent & TSubComponent = () => {
 	const ref_self = useRef<HTMLElement>(null);
 
-	const listCard: TProgramCard[] = LIST_PROGRAM_INFO.map((cardInfo, index) => {
-		return {
-			id: index,
-			width: WIDTH_ITEM,
-			imgSrc: cardInfo.imgSrc,
-			imgAlt: cardInfo.imgAlt,
-			title: cardInfo.title,
-		} as TProgramCard;
-	});
-
-	useIsomorphicLayoutEffect(() => {
-		const context = gsap.context(() => {
-			const timeline = gsap.timeline();
-			for (let index = ITEM_IN_ROW; index < listCard.length; index++) {
-				const card = listCard[index];
-				// gsap.set(`#card-${card.id}`, {
-				// 	y: "100%",
-				// });
-				timeline.to(`#card-${card.id}`, {
-					yPercent: -100,
-				});
-			}
-			gsap.registerPlugin(ScrollTrigger);
-			console.log("ref_self.current?.scrollHeight", ref_self.current!.scrollHeight + 240);
-			console.log("ref_self.current?.offsetHeight", ref_self.current?.offsetHeight);
-			ScrollTrigger.create({
-				trigger: ref_self.current,
-				animation: timeline,
-				scrub: true,
-				start: "top 5%",
-				end: () => `+=${ref_self.current?.scrollHeight}`,
-				pin: true,
-				invalidateOnRefresh: true,
-			});
-		});
-		return () => context.revert();
-	}, []);
-
 	return (
 		<section
 			id="program-group"
-			className="program-group-container flex flex-col my-high"
 			ref={ref_self}>
-			<div className="title-container flex items-center justify-center w-40 h-10 border rounded-full border-black my-8 mx-8">
-				<span className="font-bold text-2xl mb-1">Chương trình</span>
-			</div>
-			<div
-				id="container-1"
-				className="flex flex-row justify-center">
-				{listCard.slice(0, ITEM_IN_ROW).map((card) => {
-					return (
-						<ProgramGroups.ProgramCard
-							key={card.id}
-							id={card.id}
-							width={WIDTH_ITEM}
-							imgSrc={card.imgSrc}
-							imgAlt={card.imgAlt}
-							title={card.title}
+			<div className="program-group-container flex flex-col my-high mx-8">
+				<div className="title-container flex items-center justify-center w-40 h-10 border rounded-full border-black my-8">
+					<span className="font-bold text-2xl mb-1">Chương trình</span>
+				</div>
+				<div className="program-sub-group-wrap">
+					<div className="program-sub-group-wrap-inner aspect-[27/5] my-10">
+						<ProgramSubGroup
+							direction={Direction.Left}
+							cardLeft={{
+								imgSrc: LIST_PROGRAM_INFO[0].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[0].imgAlt,
+								title: LIST_PROGRAM_INFO[0].title,
+							}}
+							cardRight={{
+								imgSrc: LIST_PROGRAM_INFO[1].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[1].imgAlt,
+								title: LIST_PROGRAM_INFO[1].title,
+							}}
 						/>
-					);
-				})}
-			</div>
-			<div
-				id="container-2"
-				className="flex flex-row justify-center">
-				{listCard.slice(ITEM_IN_ROW).map((card) => {
-					return (
-						<ProgramGroups.ProgramCard
-							key={card.id}
-							id={card.id}
-							width={WIDTH_ITEM}
-							imgSrc={card.imgSrc}
-							imgAlt={card.imgAlt}
-							title={card.title}
+					</div>
+					<div className="program-sub-group-wrap-inner aspect-[27/5] my-10">
+						<ProgramSubGroup
+							direction={Direction.Right}
+							cardLeft={{
+								imgSrc: LIST_PROGRAM_INFO[2].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[2].imgAlt,
+								title: LIST_PROGRAM_INFO[2].title,
+							}}
+							cardRight={{
+								imgSrc: LIST_PROGRAM_INFO[3].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[3].imgAlt,
+								title: LIST_PROGRAM_INFO[3].title,
+							}}
 						/>
-					);
-				})}
+					</div>
+					<div className="program-sub-group-wrap-inner aspect-[27/5] my-10">
+						<ProgramSubGroup
+							direction={Direction.Left}
+							cardLeft={{
+								imgSrc: LIST_PROGRAM_INFO[4].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[4].imgAlt,
+								title: LIST_PROGRAM_INFO[4].title,
+							}}
+							cardRight={{
+								imgSrc: LIST_PROGRAM_INFO[5].imgSrc,
+								imgAlt: LIST_PROGRAM_INFO[5].imgAlt,
+								title: LIST_PROGRAM_INFO[5].title,
+							}}
+						/>
+					</div>
+				</div>
 			</div>
 		</section>
 	);
