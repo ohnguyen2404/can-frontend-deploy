1c8498bd16fd9811d071d6acef8354fed7812b29
Huy Pham <pham.giahuy.9041@gmail.com>
2023-03-05 01:19:30 +0700

    CAN-38 Update responsive Banner.

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -11,16 +11,16 @@ const Banner: TComponent & TSubComponent = () => {
 			<div className="banner-container relative">
 				<video
 					src="assets/videos/canada-video.mp4"
-					className="w-full"
+					className="object-cover w-full h-[60vh] sm:h-fit"
 					autoPlay
 					muted
 					loop
 					playsInline></video>
-				<div className="sash-ribbon-wrap w-full h-[10%] bg-white absolute bottom-[10%] flex flex-row overflow-hidden">
+				<div className="sash-ribbon-wrap w-full h-[7.5%] sm:h-[10%] bg-white absolute bottom-[10%] flex flex-row overflow-hidden">
 					<Banner.SashRibbon key={1} />
 					<Banner.SashRibbon key={2} />
 				</div>
-				<div className="title-container flex flex-col text-white absolute top-[20%] right-[10%] text-4xl font-medium">
+				<div className="title-container flex flex-col text-white absolute top-[30%] sm:top-[20%] left-[20%] sm:left-auto right-auto sm:right-[10%] text-xl sm:text-4xl sm:text- font-medium">
 					<span className="title">Start your new journey</span>
 					<span className="title">in Canada with</span>
 					<span className="title">Can Immigration Vietnam</span>
