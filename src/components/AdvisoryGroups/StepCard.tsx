2431103ebd72cabd3eb907eb78e3d0bb50525bf4
Huy Pham <pham.giahuy.9041@gmail.com>
2023-03-09 04:14:38 +0700

    CAN-42 Update Step Card and Steps Row component responsive.

--- src/components/AdvisoryGroups/StepCard.tsx
+++ src/components/AdvisoryGroups/StepCard.tsx
@@ -10,10 +10,10 @@ const StepCard = (props: TStepCard) => {
 	return (
 		<div className="step-card-container grid grid-rows-2">
 			<div className="title-container flex mx-4 items-center">
-				<span className="order-container flex items-center justify-center pb-1 h-8 w-8 text-2xl font-medium rounded-full border-black border">{props.order + 1}</span>
-				<span className="title font-medium text-xl text-center mx-3">{props.title}</span>
+				<span className="order-container flex items-center justify-center pb-0.5 md:pb-1 min-h-[18px] md:min-h-[32px] min-w-[18px] md:min-w-[32px] text-xs md:text-xl font-medium rounded-full border-black border">{props.order + 1}</span>
+				<span className="title font-medium text-xs md:text-xl text-center mx-3">{props.title}</span>
 			</div>
-			<div className="image-container h-24 mt-8">
+			<div className="image-container h-20 md:h-24 mt-0 md:mt-5">
 				<img
 					className="h-full object-contain mx-auto"
 					src={props.imgSrc}
--- src/components/AdvisoryGroups/StepsRow.tsx
+++ src/components/AdvisoryGroups/StepsRow.tsx
@@ -12,8 +12,8 @@ const StepsRow = (props: TStepRows) => {
 	const context = useContext(AdvisoryContext);
 
 	return (
-		<div className={`step-row-container grid grid-cols-8 gap-4 py-10 px-8 ${context?.color === "pink" ? "odd:bg-lightPink" : "odd:bg-lightBlue"} even:bg-white border-silver border-b last:border-b-0 first:rounded-tl-[30px] last:rounded-bl-[30px] last:rounded-br-[30px]`}>
-			<div className="col-span-1 text-2xl font-medium">*</div>
+		<div className={`step-row-container grid grid-cols-6 md:grid-cols-8 gap-4 pb-6 md:pb-10 px-15 md:px-8 ${context?.color === "pink" ? "odd:bg-lightPink" : "odd:bg-lightBlue"} even:bg-white border-silver border-b last:border-b-0 first:rounded-tl-[30px] last:rounded-bl-[30px] last:rounded-br-[30px]`}>
+			<div className="hidden md:block col-span-1 text-2xl font-medium">*</div>
 			<div className="col-span-6 grid grid-cols-6 gap-4">
 				{props.steps.map((step, idx) => (
 					<div
