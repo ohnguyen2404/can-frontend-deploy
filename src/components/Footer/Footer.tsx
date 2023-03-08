7ba5213bb3559d00a336556c5279cc4ffe7e6d1f
Huy Pham <pham.giahuy.9041@gmail.com>
2023-03-08 21:08:10 +0700

    CAN-29 Update Footer component responsive lg md screen.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
 
 const Footer = () => {
-	const title = "text-base font-bold uppercase text-strongBlue";
+	const title = "text-base font-bold whitespace-nowrap uppercase text-strongBlue";
 	const info = "font-semibold text-base";
 
-	const iconContainer = "flex w-6 h-6 ml-4 relative";
+	const iconContainer = "flex w-6 h-6 mr-4 md:ml-4 relative";
 
 	const vnAddressInfo = "Suite 312, Block B - Charmington La Pointe, 181 Cao Thắng, Phường 12, Quận 10, TP. HCM, Việt Nam";
 	const caAddressInfo = "2600 John Street, Markham, ON L3R 3W3, Canada";
 	return (
 		<section id="footer">
 			<div className="footer-container-wrap bg-lightBlue rounded-t-[30px] mt-17.5 md:mt-25">
-				<div className="footer-container grid grid-cols-8 md:grid-cols-5 grid-rows-3 md:grid-rows-2 md:gap-8 py-10 mx-15 md:mx-8 md:pr-20">
-					<div className="sub-section col-span-2 md:col-span-1 row-span-1 md:row-span-full justify-items-start">
-						<div className="icon-container relative w-1/2 h-full">
+				<div className="footer-container grid grid-cols-8 lg:grid-cols-5 grid-rows-5 lg:grid-rows-2 gap-4 md:gap-8 py-10 mx-15 md:mx-8 lg:pr-20">
+					<div className="sub-section col-span-2 lg:col-span-1 justify-items-start">
+						<div className="icon-container relative w-3/4 lg:w-1/2 h-full">
 							<Image
 								src="/logo/logo_red.svg"
 								alt="logo"
@@ -24,13 +24,16 @@ const Footer = () => {
 							/>
 						</div>
 					</div>
-					<div className="sub-section md:col-span-2">
-						<div className="title-container text-4xl font-bold uppercase text-strongPink">Nhận visa Canada cực dễ cùng Can-Immigration</div>
+					<div className="sub-section col-span-5 lg:col-span-2">
+						<div className="title-container text-2xl md:text-4xl font-bold uppercase text-strongPink">Nhận visa Canada cực dễ cùng Can-Immigration</div>
 					</div>
-					<div className="sub-section md:col-span-2">
-						<div className="description-container text-xl font-semibold">Với 30 năm kinh nghiệm tư vấn định cư và du học Canada, chúng tôi tự tin sẽ hỗ trợ các bạn đạt được tấm thẻ PR mà bạn mong muốn.</div>
+					<div className="sub-section col-start-3 lg:col-start-auto row-start-2 lg:row-start-auto col-span-5 lg:col-span-2">
+						<div className="description-container text-base md:text-xl font-semibold">Với 30 năm kinh nghiệm tư vấn định cư và du học Canada, chúng tôi tự tin sẽ hỗ trợ các bạn đạt được tấm thẻ PR mà bạn mong muốn.</div>
 					</div>
-					<div className="sub-section md:col-start-2 md:row-start-2">
+					<div className="sub-section lg:hidden relative row-start-3 col-span-full">
+						<div className="separate-line absolute h-[1px] w-full bg-strongBlue top-1/2"></div>
+					</div>
+					<div className="sub-section row-start-4 lg:row-start-auto lg:col-start-2 col-span-5 lg:col-span-1">
 						<div className="vn-address-container">
 							<div className={`title ${title}`}>Văn Phòng Việt Nam</div>
 							<a
@@ -41,8 +44,8 @@ const Footer = () => {
 							</a>
 						</div>
 					</div>
-					<div className="sub-section">
-						<div className="ca-address-container">
+					<div className="sub-section relative lg:static row-start-5 lg:row-start-auto col-span-5 lg:col-span-1">
+						<div className="ca-address-container absolute lg:static bottom-0 lg:bottom-auto">
 							<div className={`title ${title}`}>Văn Phòng canada</div>
 							<a
 								href={`https://www.google.com/maps?q=${caAddressInfo}`}
@@ -52,7 +55,7 @@ const Footer = () => {
 							</a>
 						</div>
 					</div>
-					<div className="sub-section">
+					<div className="sub-section row-start-4 lg:row-start-auto col-start-7 lg:col-start-auto col-span-2 lg:col-span-1">
 						<div className="phone-container">
 							<div className="settle-phone-container">
 								<div className={`title ${title}`}>Tư vấn định cư</div>
@@ -72,8 +75,8 @@ const Footer = () => {
 							</div>
 						</div>
 					</div>
-					<div className="sub-section flex justify-end">
-						<div className="social-container flex flex-row self-end">
+					<div className="sub-section relative row-start-5 lg:row-start-auto col-start-7 lg:col-start-auto col-span-2 lg:col-span-1">
+						<div className="social-container flex absolute bottom-0">
 							<div className="gmail-container">
 								<a
 									className={`icon-container ${iconContainer}`}
