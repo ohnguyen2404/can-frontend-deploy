5c84bb7f18b8893b22813f619b1ce867eda404f5
Huy Pham <pham.giahuy.9041@gmail.com>
2023-03-09 03:34:53 +0700

    CAN-42 Update Step Card component layout.

--- src/components/AdvisoryGroups/StepCard.tsx
+++ src/components/AdvisoryGroups/StepCard.tsx
@@ -8,14 +8,14 @@ export type TStepCard = {
 
 const StepCard = (props: TStepCard) => {
 	return (
-		<div className="step-card-container grid grid-rows-4">
-			<div className="title-container row-span-1 flex mx-4 items-center">
+		<div className="step-card-container grid grid-rows-2">
+			<div className="title-container flex mx-4 items-center">
 				<span className="order-container flex items-center justify-center pb-1 h-8 w-8 text-2xl font-medium rounded-full border-black border">{props.order + 1}</span>
 				<span className="title font-medium text-xl text-center mx-3">{props.title}</span>
 			</div>
-			<div className="image-container row-span-3 mt-8">
+			<div className="image-container h-24 mt-8">
 				<img
-					className="h-28 object-contain mx-auto"
+					className="h-full object-contain mx-auto"
 					src={props.imgSrc}
 				/>
 			</div>
